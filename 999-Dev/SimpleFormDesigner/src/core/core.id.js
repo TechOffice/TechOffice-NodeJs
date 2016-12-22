c16dfdd99c1db498440bbeed93f8f35aca360118
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-12-22 14:10:16 +0800

    update

--- 999-Dev/SimpleFormDesigner/src/core/core.id.js
+++ 999-Dev/SimpleFormDesigner/src/core/core.id.js
@@ -1,21 +1,21 @@
 module.exports = function(designer){
-	
+
 	var idPrefix = "ID";
-	
+
 	var id = function(){
-		
+
 	}
-	
+
 	id.currentSeq = 0;
-	
-	id.getCurrentId(){
+
+	id.getCurrentId = function(){
 		return idPrefix + id.currentSeq;
-	}
-	
-	id.getNextId(){
+	};
+
+	id.getNextId = function(){
 		id.currentSeq = id.currentSeq + 1;
 		return idPrefix + id.currentSeq;
 	}
-	
+
 	designer.id = id;
 }
