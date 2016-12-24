d52fe2b7dc9aa4a10b05b2b520770dbdd5a4697d
Ben <imben1109@gmail.com>
2016-12-24 11:37:01 +0800

    Revert "update"
    This reverts commit b7b7db2f98cbc43757a3df52b5239436d7d5fcea.

--- 999-Dev/SimpleFormDesigner/src/designer.js
+++ 999-Dev/SimpleFormDesigner/src/designer.js
@@ -1,7 +1,3 @@
-/*jslint node: true */
-/*global designer:true*/
-"use strict";
-
 designer = require("./core/core.js")();
 require("./core/core.controlPanel.js")(designer);
 require("./core/core.drawingPanel.js")(designer);
