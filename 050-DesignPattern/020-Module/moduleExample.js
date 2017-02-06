68c8759edbf198a2467fbc1ac445135181c0a59c
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-06 14:48:24 +0800

    Update

---
+++ 050-DesignPattern/020-Module/moduleExample.js
@@ -0,0 +1,21 @@
+var module = (function(){
+	var name;
+	var age;
+	return {
+		getName: function(){
+			return name;
+		},
+		setName: function(lName){
+			name = lName;
+		},
+		getAge: function(){
+			return age;
+		},
+		setAge: function(lAge){
+			age = lAge;
+		}
+	}
+})();
+
+module.setName("Testing");
+console.log(module.getName());
---
+++ 050-DesignPattern/030-Singleton/README.md
@@ -0,0 +1,2 @@
+# Singleton 
+It can be implemeneted by created a class with a method that create a new instance of class if one doesn't exit. 
